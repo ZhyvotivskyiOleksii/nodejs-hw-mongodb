572aaded86c157d92ea19e7cc4c65dc4856e500b
Oleksii Zhyvotivskyi <gamesdashev@gmail.com>
2025-06-21 23:00:59 +0200

    fix: обробка 404, валідація ObjectId, сортування, пагінація

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@
 export const errorHandler = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || 'Something goes wrong';
-
+//stat
   res.status(status).json({
     status,
     message,
